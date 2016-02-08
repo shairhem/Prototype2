6c4c34d3bdecf5dbc12e26f72ca5ae508b29a64f
shairhem <shairhem@gmail.com>
2016-02-08 22:30:36 +0800

    Fix button1 alignment

--- Prototype2/Form2.Designer.cs
+++ Prototype2/Form2.Designer.cs
@@ -51,7 +51,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(242, 73);
+            this.button1.Location = new System.Drawing.Point(239, 86);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
