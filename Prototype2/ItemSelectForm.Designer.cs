c1704a4dddeaefea0c08615bcd06c6dd79712e56
shairhem <shairhem@gmail.com>
2016-02-08 22:24:36 +0800

    Disable test mode

--- Prototype2/ItemSelectForm.Designer.cs
+++ Prototype2/ItemSelectForm.Designer.cs
@@ -78,6 +78,7 @@
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
+            this.radioButton1.Enabled = false;
             this.radioButton1.Location = new System.Drawing.Point(38, 73);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(76, 17);
